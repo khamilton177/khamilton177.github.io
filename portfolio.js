809c293dff7561d57b958b1a649f1d373fc00f96
KHamilton177 <kerry.hamilton177@gmail.com>
2017-07-30 18:31:25 -0400

    try pointer event

--- portfolio.js
+++ portfolio.js
@@ -152,7 +152,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
   for (cnt=0; projContainer.length > cnt; cnt++){
     //  show modal when hover over container
-    projContainer[cnt].addEventListener("mouseenter", function(event){
+    projContainer[cnt].addEventListener("pointerenter", function(event){
       event.preventDefault();
       var modalName=event.target.id;
       // console.log("name "+modalName);
